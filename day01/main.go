8b94caa1c1a64f8923cb32f367753ae1377c9de1
Shivan Sivakumarnan <51132467+ShivanS93@users.noreply.github.com>
2021-12-01 22:47:58 +1300

    day 1 completed - no refactor

--- day01/main.go
+++ day01/main.go
@@ -2,10 +2,81 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("test")
-	a := 100
-	b                     := 100
+	// problemOne()
+	problemTwo()
+}
+
+func problemOne() {
+	// opening file
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	counter := -1 // set to -1 because counter will be off by one
+	var prev int64
+	prev = 0
+	// iterating through lines
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		num, _ := strconv.ParseInt(scanner.Text(), 10, 32)
+		if prev < num {
+			counter = counter + 1
+		}
+		prev = num
+	}
+	fmt.Println("Answer to problem one: ")
+	fmt.Println(counter)
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func problemTwo() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	// turning text into slice of intergers
+	scanner := bufio.NewScanner(file)
+	var scans []int64
+	for scanner.Scan() {
+		num, _ := strconv.ParseInt(scanner.Text(), 10, 32)
+		scans = append(scans, num)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	// turning rolling sum into another slice
+	var rolling_values []int64
+	for i, val := range scans {
+		if i < len(scans)-2 {
+			sum := val + scans[i+1] + scans[i+2]
+			rolling_values = append(rolling_values, sum)
+		}
+	}
+
+	counter := 0 // count the number of increases
+	for i, val := range rolling_values {
+		if i > 0 {
+			if val > rolling_values[i-1] {
+				counter++
+			}
+		}
+	}
+	fmt.Println("Answer to problem two: ")
+	fmt.Println(counter)
 }
